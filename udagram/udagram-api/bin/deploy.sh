d4d006b68fdfc9816207c5c22c55f2045dc16088
Tamer Ismail <tamer.ismail1@gmail.com>
2022-10-28 21:13:07 +0200

    Update deploy.sh

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 eb init udagram-api --platform node.js --region us-east-1
 eb list
 eb use udagram-api-env 
-eb setenv POSTGRES_HOST=$POSTGRES_HOST POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_DB=$POSTGRES_DB POSTGRES_PASSWORD=$POSTGRES_PASSWORD PORT=$PORT AWS_REGION=$AWS_DEFAULT_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET
+eb setenv POSTGRES_HOST=postgres.cae3b41kchxk.us-east-1.rds.amazonaws.com POSTGRES_USERNAME=postgres POSTGRES_DB=postgres POSTGRES_PASSWORD=password PORT=8080 AWS_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=devtamer URL=http://udagram-api-env.eba-3y8khtzp.us-east-1.elasticbeanstalk.com JWT_SECRET=MyJWTSecret
 eb deploy
