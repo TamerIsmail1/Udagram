4843967350c91f83435892d5cf89e4b6766fd305
‪Tamer Ismail‬‏ <42833418+TamerIsmail1@users.noreply.github.com>
2022-10-28 21:47:41 +0200

    Delete dev.sh

--- udagram/udagram-api/bin/dev.sh
+++
@@ -1,5 +0,0 @@
-eb init udagram-api --platform node.js --region us-east-1
-eb list
-eb use udagram-api-env
-eb setenv POSTGRES_HOST=postgres.cae3b41kchxk.us-east-1.rds.amazonaws.com POSTGRES_USERNAME=postgres POSTGRES_DB=postgres POSTGRES_PASSWORD=password PORT=8080 AWS_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=devtamer URL=http://udagram-api-env.eba-3y8khtzp.us-east-1.elasticbeanstalk.com JWT_SECRET=MyJWTSecret
-eb deploy
